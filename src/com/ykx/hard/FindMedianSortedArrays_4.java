85e79d4a239378702c2a46ee624db34c1f9f22fd
MuYi <1186965411@qq.com>
2019-12-04 12:28:33 +0800

    4 寻找两个有序数组的中位数

--- src/com/ykx/hard/FindMedianSortedArrays_4.java
+++ src/com/ykx/hard/FindMedianSortedArrays_4.java
@@ -73,13 +73,97 @@ public class FindMedianSortedArrays_4 {
         else
             return right;
     }
+
+    /**
+     * 上边的两种思路，时间复杂度都达不到题目的要求 O(log(m+n)O(log(m+n)。
+     * 看到 log，很明显，我们只有用到二分的方法才能达到。
+     * 我们不妨用另一种思路，题目是求中位数，其实就是求第 k 小数的一种特殊情况，而求第 k 小数有一种算法。
+     *
+     * 解法二中，我们一次遍历就相当于去掉不可能是中位数的一个值，也就是一个一个排除。
+     * 由于数列是有序的，其实我们完全可以一半儿一半儿的排除。
+     * 假设我们要找第 k 小数，我们可以每次循环排除掉 k/2 个数。
+     *
+     * 时间复杂度：每进行一次循环，我们就减少 k/2 个元素，所以时间复杂度是 O(log(k)，
+     * 而 k=(m+n)/2，所以最终的复杂也就是 O(log(m+n）。
+     *
+     * 空间复杂度：虽然我们用到了递归，但是可以看到这个递归属于尾递归，
+     * 所以编译器不需要不停地堆栈，所以空间复杂度为 O(1)O(1)。
+     *
+     * https://leetcode-cn.com/problems/median-of-two-sorted-arrays/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-w-2/
+     *
+     */
+    public double findMedianSortedArrays3(int[] nums1,int[] nums2){
+        int m=nums1.length,n=nums2.length;
+        int left=(m+n+1)/2,right=(m+n+2)/2;//将奇数和偶数的情况合并，如果是奇数求出的left=right
+        return (getThk(nums1,0,m-1,nums2,0,n-1,left)+getThk(nums1,0,m-1,nums2,0,n-1,right))/2;
+    }
+    //利用递归的方式求两个数组合起来中第k小的数
+    public double getThk(int[] nums1,int start1,int end1,int[] nums2,int start2,int end2,int k){
+        int len1=end1-start1+1,len2=end2-start2+1;
+        if(len1>len2) //确保len1（即nums1）是较小的那个数组，这样如果有数组空了，一定是nums1
+            return getThk(nums2,start2,end2,nums1,start1,end1,k);
+        if(len1==0)//数组nums1为空时，第k小的数就在nums2的第k位置
+            return nums2[start2+k-1];
+        if(k==1)//当k=1时，第1小的数为nums[1]和nums[1]中较小的那一个
+            return Math.min(nums1[start1],nums2[start2]);
+
+        int i=start1+Math.min(len1,k/2)-1;
+        int j=start2+Math.min(len2,k/2)-1;
+        if(nums1[i]>nums2[j])//舍弃nums2[j]左边的部分（较小的那一部分）
+            return getThk(nums1,start1,end1,nums2,j+1,end2,k-(j+1-start2));
+        else//舍弃nums2[i]左边的部分
+            return getThk(nums1,i+1,end1,nums2,start2,end2,k-(i+1-start1));
+    }
+
+
+    /**
+     *https://leetcode-cn.com/problems/median-of-two-sorted-arrays/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-w-2/
+     */
+    public double findMedianSortedArrays4(int[] A, int[] B) {
+        int m = A.length;
+        int n = B.length;
+        if (m > n) {
+            return findMedianSortedArrays4(B,A); // 保证 m <= n
+        }
+        int iMin = 0, iMax = m;
+        while (iMin <= iMax) {
+            int i = (iMin + iMax) / 2;
+            int j = (m + n + 1) / 2 - i;
+            if (j != 0 && i != m && B[j-1] > A[i]){ // i 需要增大
+                iMin = i + 1;
+            }
+            else if (i != 0 && j != n && A[i-1] > B[j]) { // i 需要减小
+                iMax = i - 1;
+            }
+            else { // 达到要求，并且将边界条件列出来单独考虑
+                int maxLeft = 0;
+                if (i == 0) { maxLeft = B[j-1]; }
+                else if (j == 0) { maxLeft = A[i-1]; }
+                else { maxLeft = Math.max(A[i-1], B[j-1]); }
+                if ( (m + n) % 2 == 1 ) { return maxLeft; } // 奇数的话不需要考虑右半部分
+
+                int minRight = 0;
+                if (i == m) { minRight = B[j]; }
+                else if (j == n) { minRight = A[i]; }
+                else { minRight = Math.min(B[j], A[i]); }
+
+                return (maxLeft + minRight) / 2.0; //如果是偶数的话返回结果
+            }
+        }
+        return 0.0;
+    }
+
     @Test
     public void test(){
-        int[] nums1=new int[]{1,3};
-        int[] nums2=new int[]{2};
+        int[] nums1=new int[]{1,2};
+        int[] nums2=new int[]{3,4};
         double res1=findMedianSortedArrays1(nums1,nums2);
         double res2=findMedianSortedArrays2(nums1,nums2);
+        double res3=findMedianSortedArrays3(nums1,nums2);
+        double res4=findMedianSortedArrays4(nums1,nums2);
         System.out.println(res1);
         System.out.println(res2);
+        System.out.println(res3);
+        System.out.println(res4);
     }
 }
